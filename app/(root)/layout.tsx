2a5b788b8fa2ed7d2aed61debd4458878f7ea753
Matteo Luigi Tommasi <m.tommasi@greenaus.it>
2025-01-23 19:11:53 +0100

    Add Header component, utility functions, and update layout structure; remove unused SVG files

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import { FC, ReactNode } from "react";
+import Header from "@/app/components/Header";
+
+ const Layout: FC<{ children: ReactNode }> = ({ children }) => {
+  return (
+    <main className="root-container">
+        <Header />
+      <div className="mx-auto mt-20 pb-20 max-w-7xl">{children}</div>
+    </main>
+  );
+};
+
+
+export default Layout;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/app/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <Button>HELLO</Button>
+      <p className="font-ibm-plex-sans">
+        IBM PLEX SANS Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Et ad doloremque recusandae eveniet eaque beatae odit optio culpa, ut
+        voluptatem asperiores unde corporis modi possimus perspiciatis quo
+        repudiandae exercitationem quasi.
+      </p>
+      <p className="font-bebas-neue">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus
+        iste libero architecto similique eum expedita esse nesciunt molestiae,
+        explicabo sequi eaque? Magni nam quasi sit veritatis eum eveniet dolorem
+        laboriosam.
+      </p>
+    </>
+  );
+}
