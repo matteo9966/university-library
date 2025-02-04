76deef9b01add5c91d95ea9c1f756da666491d16
Matteo Luigi Tommasi <m.tommasi@greenaus.it>
2025-02-04 09:06:05 +0100

    Add authentication middleware, update user roles and status enums, and enhance Tailwind CSS configuration

--- app/(auth)/seed/route.ts
+++ app/(auth)/seed/route.ts
@@ -1,6 +1,14 @@
 import sql from "@/lib/db";
 import { responseBuilder } from "@/lib/responseBuilder";
 
+
+async function createUserRolesEnum() {
+  await sql`CREATE TYPE user_role AS ENUM ('user', 'admin')`;
+}
+async function createUserStatusEnum(){
+  await sql`CREATE TYPE user_status AS ENUM ('pending','subscribed')`;
+}
+
 async function createUserTable() {
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
   await sql`
@@ -10,13 +18,17 @@ async function createUserTable() {
         password VARCHAR(255) NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        univerisityidnumber VARCHAR(255) NOT NULL
+        univerisityidnumber VARCHAR(255) NOT NULL,
+        role user_role DEFAULT 'user'::user_role,
+        status user_status DEFAULT 'pending'::user_status
     )`;
 }
 
 export async function GET() {
   try {
     await sql`BEGIN`;
+    await createUserRolesEnum();
+    await createUserStatusEnum();
     await createUserTable();
     await sql`COMMIT`;
     return Response.json(
